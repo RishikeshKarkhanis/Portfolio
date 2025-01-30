2ab01e50532baf0efeca73071a5372c1c5d5f04f
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-01-30 15:40:08 +0530

    Home Page Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+
+      <div className="contentPage">
+        <img src="ProfilePicture" alt="Rishikesh Karkhanis" srcset="/public/images/linkedin_profile.png" />
+        <div className="desc">
+          <h1>I'm "Rishikesh Karkhanis"</h1>
+          <p>Student @ Acropolis Institute Of Technology & Research, Indore</p>
+          <p>Pursuing B-Tech (CSE-AIML)</p>
+        </div>
+      </div>
+
+      <footer>
+        <a href="/about">Next</a>
+      </footer>
     </div>
   );
 }
