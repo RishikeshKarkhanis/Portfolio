44ca67bcb9f04b89f29062e0b8f84d8824668b6e
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-01-30 18:26:51 +0530

    Social Links Page Done

--- src/pages/Social_Links/SocialLinks.jsx
+++ src/pages/Social_Links/SocialLinks.jsx
@@ -5,6 +5,26 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+
+      <div className="contentPage">
+        <h1>Social Links</h1>
+
+        <div className="rootContainer">
+          <div className="gridContainer">
+            <a className='logo' href="https:/www.instagram.com/RishikeshKarkhanis"><img src="Logo" alt="Logo" srcset="/public/images/instagram.png" /></a>
+            <a className='logo' href="https:/www.linkedin.com/in/rishikeshkarkhanis0101"><img src="Logo" alt="Logo" srcset="/public/images/linkedin.png" /></a>
+            <a className='logo' href="https:/www.github.com/RishikeshKarkhanis"><img src="Logo" alt="Logo" srcset="/public/images/github.png" /></a>
+            <a className='logo' href="https://x.com/RishikeshK0101"><img src="Logo" alt="Logo" srcset="/public/images/twitter.png" /></a>
+          </div>
+        </div>
+
+      </div>
+
+      <footer>
+        <a href="/projects">Back</a>
+        <a href="/contacts">Next</a>
+      </footer>
+
     </div>
   );
 }
