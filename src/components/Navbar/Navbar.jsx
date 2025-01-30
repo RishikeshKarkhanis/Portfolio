73f4fa6d4acb4ce1731de3cea505d210eaf81bdc
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-01-30 14:50:07 +0530

    Local Setup Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <div className="Navbar">
-      <button className="title">Rishikesh K.</button>
+      <button className="title">Rishikesh Karkhanis</button>
       <button className="options">=</button>
     </div>
   );
