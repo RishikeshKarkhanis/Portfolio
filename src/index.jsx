c171f8adef44d7724aca6cf8d795bb963b602965
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-01-30 19:42:06 +0530

    Changed BrowserRouter Basename

--- src/index.jsx
+++ src/index.jsx
@@ -9,10 +9,11 @@ import SocialLinks from './pages/Social_Links/SocialLinks';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://rishikeshkarkhanis.github.io/Portfolio/">
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
