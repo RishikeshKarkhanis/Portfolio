22b2739edc30a35ba7be9966df2967ba8f2dd640
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-02-12 02:56:13 +0530

    Made Contact System Functional

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,31 @@
 import './Contact.css';
 import Navbar from '../../components/Navbar/Navbar';
 
+function SendMail() {
+  const email = import.meta.env.VITE_EMAIL;
+  const pass = import.meta.env.VITE_PASS;
+  const msg = document.getElementById('msg').value;
+  const sender = document.getElementById('email').value;
+
+  console.log(email);
+  console.log(pass);
+  
+  
+    
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: email,
+    Password: pass,
+    To: 'rishikeshkarkhanis0101@gmail.com',
+    From: sender,
+    Subject: "Message From Portfolio",
+    Body: msg,
+  })
+    .then(function (message) {
+      alert("mail sent successfully")
+    });
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,11 +34,11 @@ function App() {
       <div className="contentPage">
         <h1>Contact Me!</h1>
 
-        <form className='contactForm'>
+        <div className='contactForm'>
           <input type="email" name="email" id="email" placeholder='Email' />
           <textarea name="msg" id="msg" cols="30" rows={10} placeholder='Enter Your Message Here'></textarea>
-          <button type="submit">Submit</button>
-        </form>
+          <button onClick={SendMail}>Submit</button>
+        </div>
 
       </div>
 
