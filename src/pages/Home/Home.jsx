e534f0188da8afe1f450e419d284f02298ae7d26
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-02-18 16:36:13 +0530

    Made Download Portfolio Option

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
 import profile from '../../../public/images/linkedin_profile.png';
+import resume from '../../../public/docs/resume.pdf';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <h1 className='nametag'>I'm "Rishikesh Karkhanis"</h1>
           <p>Student @ Acropolis Institute Of Technology & Research, Indore</p>
           <p>Pursuing B-Tech (CSE-AIML)</p>
+          <a className='resume' href={resume} target='_blank'>Download Resume</a>
         </div>
       </div>
 
